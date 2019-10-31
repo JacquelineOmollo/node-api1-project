732ab1cd4fcbab4fbf0f08428289762c0aa669e4
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-10-31 00:41:17 -0400

    MVP done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 //bring express into project
 const db = require("./data/db");
-server.use(express.json());
+// server.use(express.json());
 //create a server object
 const server = express();
 
@@ -15,20 +15,24 @@ server.listen(port, () => {
   console.log("\n=== server listening on port 8000 ===\n");
 });
 
-server.post("/apt/users", (req, res) => {
-  const hubInfo = req.body;
-  // console.log('body:', hubInfo);
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+  const { name, bio } = userInfo;
 
-  db.add(hubInfo)
+  if (!name || !bio) {
+    res.status(400).json({
+      errorMessage: "Please provide name and bio for the user."
+    });
+  }
+
+  db.insert(userInfo)
     .then(hub => {
-      res.status(201).json({ success: true, hub });
+      console.log(hub);
+      res.status(201).json({ succuss: true, userInfo });
     })
-    .catch(err => {
-      res.status(400).json({
-        errorMessage: "Please provide name and bio for the user.",
-        success: false,
-        err
-      });
+    .catch(error => {
+      console.log("error", error);
+      res.status(500).json({ error: "failed to post data" });
     });
 });
 
@@ -38,24 +42,30 @@ server.get("/api/users", (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
+      console.log("error", error);
       res.status(500).json({
-        error: "The users information could not be retrieved.",
-        success: false,
-        err
+        error: "The users information could not be retrieved."
       });
     });
 });
 
 server.get("/api/users/:id", (req, res) => {
-  const id = req.params;
+  const id = req.params.id;
   db.findById(id)
     .then(users => {
-      res.status(200).json(users);
+      if (!users) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.status(200).json(users);
+      }
     })
-    .catch(message => {
+    .catch(error => {
+      console.log("error", error);
       res
-        .status(404)
-        .json({ message: "The user with the specified ID does not exist." });
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
     });
 });
 
@@ -66,40 +76,44 @@ server.delete("/api/users/:id", (req, res) => {
   db.remove(id)
     .then(deletedHub => {
       if (deletedHub) {
-        res.status(204).end();
-      } else {
         res
           .status(404)
           .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.status(204).json({ message: `User was with ${id} was deleted` });
       }
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({ error: "The user could not be removed", success: false, err });
+      res.status(500).json({ error: "The user could not be removed" });
     });
 });
 
 server.put("/api/users/:id", (req, res) => {
-  const id = req.params.id;
-  const hubInfo = req.body;
+  const id = Number(req.params.id);
+  const Info = req.body;
+  const { name, bio } = Info;
 
-  db.update(id, hubInfo)
-    .then(hub => {
-      if (hub) {
-        res.status(200).json({ success: true, hub });
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+
+  db.update(id, Info)
+    .then(data => {
+      console.log(data);
+      if (data === 0) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
       } else {
-        res.status(404).json({
-          success: false,
-          message: "The user with the specified ID does not exist."
-        });
+        res.status(200).json(info);
       }
     })
     .catch(err => {
-      res.status(500).json({
-        error: "The user information could not be modified.",
-        success: false,
-        err
-      });
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The user information could not be modified." });
     });
 });
